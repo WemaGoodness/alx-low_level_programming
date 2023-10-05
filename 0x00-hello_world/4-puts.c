a331146f2b2a4294726e4edacea5450bd40964f1
WemaGoodness <goodnesswema20@gmail.com>
2023-10-05 07:03:11 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
-	/*string initialisation*/
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-    puts(Mystr); /*writing the string to stdout*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
 
     return 0;
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 
 {
 	printf("Size of a char: %d-byte(s)\n", sizeof(char));
