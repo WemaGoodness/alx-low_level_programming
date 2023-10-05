ab57c78d729e805eb83fa5a7dd5e2afab2905531
WemaGoodness <goodnesswema20@gmail.com>
2023-10-05 06:56:17 -0700

    C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+
+{
+	printf("Size of a char: %d-byte(s)\n", sizeof(char));
+	printf("Size of an int: %d-byte(s)\n", sizeof(int));
+	printf("Size of floating-point: %d-byte(s)\n", sizeof(float));
+	printf("Size of long int: %d-byte(S)\n", sizeof(long int))
+	printf("Size of long long int: %d-byte(s)\n", sizeof(long long int));
+	printf("Size of double: %d-byte(s)\n", sizeof(double));
+	
+	return 0;
+}
