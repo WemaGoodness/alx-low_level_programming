1ca2f77d8e039b29e63132c481bc12d01782c6da
WemaGoodness <goodnesswema20@gmail.com>
2023-10-05 07:05:45 -0700

    C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
@@ -16,5 +15,5 @@ int main(void)
 	printf("Size of long long int: %d-byte(s)\n", sizeof(long long int));
 	printf("Size of double: %d-byte(s)\n", sizeof(double));
 	
-	return 0;
+	return (0);
 }
