369afb125ac559aad76142a9239619bd67faa9e5
WemaGoodness <goodnesswema20@gmail.com>
2023-10-06 09:08:18 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the smallest combination of two digits
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
