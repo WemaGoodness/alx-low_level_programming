4c94e8f98b7224d2606970782bc0e1bd503756d3
WemaGoodness <goodnesswema20@gmail.com>
2023-10-11 04:40:31 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isalpha - Checks for alphabetic character
+  * @c: character to be checked
+  *
+  * Return: Always (1) or (0)
+  */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
