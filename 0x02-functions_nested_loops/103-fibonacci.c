cb290be67ca6717dc4e2ba246203a5ca88475248
WemaGoodness <goodnesswema20@gmail.com>
2023-10-11 06:15:14 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - Prints sum of even Fibonacci numbers < 4000000
+  *
+  * Return: void
+  */
+int main(void)
+{
+	int f = 0;
+	long i = 1, b = 2, sum = b;
+
+	do
+	{
+		b += i;
+
+		if (b % 2 == 0)
+			sum += b;
+
+		i = b - i;
+
+		++f;
+	} while (k + j < 4000000);
+
+	printf("%ld\n", sum);
+	return (0);
+}
