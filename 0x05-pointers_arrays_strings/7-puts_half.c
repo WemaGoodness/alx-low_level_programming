8139087d4cc2e322a2e6c1415d0b4b4458d2cb5d
WemaGoodness <goodnesswema20@gmail.com>
2023-10-16 13:06:54 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,13 +15,13 @@ void puts_half(char *str)
 		length++;
 	}
 
-	if (length % 2 != 0)
+	if (length % 2 != 1)
 	{
 		mid = (length - 1) / 2;
 	}
 	else 
 	{
-		mid = (length + 1) / 2;
+		mid = length / 2;
 	}
 
 	while (str[mid] != '\0')
