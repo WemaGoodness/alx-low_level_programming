8e37e8fc828d1ed1d00c9d4c2ee88611ecfd136f
WemaGoodness <goodnesswema20@gmail.com>
2023-10-16 07:59:58 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,18 +2,11 @@
 
 /**
  * reset_to_98 - updates value to 98
- * @*n: pointer to update value
+ * @n: pointer to update value
  *
  * Return: Always 0
  */
 void reset_to_98(int *n)
 {
-	int p;
-
-	p = 402;
-	n = &p;
-	_putchar("%d", p);
-
-	*n = 98;
-	_putchar("%d", p);
+	*n = 98
 }
