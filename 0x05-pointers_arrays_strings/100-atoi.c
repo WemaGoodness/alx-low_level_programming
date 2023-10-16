db747605d071d084eebe6dd2c481fec2544b4cf1
WemaGoodness <goodnesswema20@gmail.com>
2023-10-16 14:18:43 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: string to convert
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int length, sign = 1;
+	int num = 0, equiv = 0;
+
+	for (i = 0; s[length] != '\0'; length++)
+	{
+		if (s[length] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[length] >= '0' && s[length] <= '9')
+		{
+			num = num * 10 + (s[length] - '0');
+			equiv = 1;
+		}
+		else if (equiv == 1)
+		{
+			break;
+		}
+	}
+
+	num *= sign;
+	return (num);
+}
