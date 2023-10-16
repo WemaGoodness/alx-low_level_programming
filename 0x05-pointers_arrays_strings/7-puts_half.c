f781cab47947a7a43c593e59ff120b673779480c
WemaGoodness <goodnesswema20@gmail.com>
2023-10-16 12:57:35 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to split
+ *
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int length = 0, mid;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		mid = (length - 1) / 2;
+	}
+	else 
+	{
+		mid = length / 2
+	}
+
+	while (str[mid] != '\0')
+	{
+		_putchar(str[mid]);
+		mid++;
+	}
+	_putchar('\n');
+}
