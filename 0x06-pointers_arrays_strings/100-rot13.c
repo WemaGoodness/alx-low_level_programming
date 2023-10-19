2e98459107472fd1cb83adf9c05f5a620f541d8b
WemaGoodness <goodnesswema20@gmail.com>
2023-10-19 23:23:47 +0300

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @input: string to encode
+ *
+ * Return: encoded string
+ */
+char *rot13(char *input)
+{
+	char *output;
+	char c, base;
+
+	if (input == NULL)
+	{
+		*output = input;
+		return NULL;
+	}
+
+	while (*input)
+	{
+		c = *input;
+
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		{
+			base = (c >= 'a') ? 'a' : 'A';
+			*input = base + ((c - base + 13) % 26);
+		}
+
+		input++;
+	}
+
+	return (output);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *s);
-
+char *rot13(char *input);
