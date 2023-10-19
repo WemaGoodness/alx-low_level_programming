25399a1a7def1b3e89c6286dee58a88736335578
WemaGoodness <goodnesswema20@gmail.com>
2023-10-19 21:00:09 +0300

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && s2 != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			return *s1 - *s2;
-		}
-
 		s1++;
 		s2++;
 	}
-	result =  *s1 - *s2
-
-	return  (result);
+	return (*s1 - *s2);
 }
