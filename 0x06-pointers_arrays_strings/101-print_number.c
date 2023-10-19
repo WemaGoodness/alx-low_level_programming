669f5ee205b984f4695d8c2b230032a6ddf832d1
WemaGoodness <goodnesswema20@gmail.com>
2023-10-19 23:54:45 +0300

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integer
+ * @n: integer to print
+ *
+ * Return: Always (0)
+ */
+void print_number(int n)
+{
+	int reversed = 0;
+        int digit;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n > 0)
+	{
+		digit = n % 10;
+		reversed = reversed * 10 + digit;
+		n = n / 10;
+	}
+
+	if (reversed == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		while (reversed > 0)
+		{
+			_putchar('0' + (reversed % 10));
+			reversed = reversed / 10;
+		}
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,6 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *s);
 char *rot13(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
