a60c352a1cb7bf7cfc72c4d241f172b3a0bc44b7
WemaGoodness <goodnesswema20@gmail.com>
2023-10-23 22:25:19 +0300

    C - More pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int row, col;
 
-	for (i = 0; i < 8; i++)
+	for (row = 0; row < 8; row++)
 	{
-		for (j = 0; j < 8; j++)
+		for (col = 0; col < 8; col++)
 		{
-			_putchar("%c", a[i][j]);
+			_putchar(a[row][col]);
 		}
 
-		_putchar("\n");
+		_putchar('\n');
 	}
 }
