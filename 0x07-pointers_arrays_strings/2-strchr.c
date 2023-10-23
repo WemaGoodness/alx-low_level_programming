0740498050105fd118566ea0c1e7d6a88ada595b
WemaGoodness <goodnesswema20@gmail.com>
2023-10-23 21:03:58 +0300

    C - More pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - finds character in string
+ * @s: string to be checked
+ * @c: character to be searched
+ *
+ * Return: Always (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+	{
+		s++;
+	}
+
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
