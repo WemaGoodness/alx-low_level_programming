8b91546016fa7717b955939166a7a68cc8c00941
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 19:37:27 +0300

    C - Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints string
+ * length
+ * @s: string to be chescked
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*S != '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
