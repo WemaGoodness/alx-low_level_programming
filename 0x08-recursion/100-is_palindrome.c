787de0fd1491d3bd0f3f2e8c3ca6b143f3dda589
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 21:41:25 +0300

    C - Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,14 +27,14 @@ int find_lindrome(char *start, char *end)
  *
  * Return: lenght of s
  */
-int _strlen(char s)
+int _strlen(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	return (_strlen(s + 1));
+	return (_strlen(*s + 1));
 }
 /**
  * is_palindrome - shows if s is a
