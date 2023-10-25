702712bc16aec216d69a602934157a1451d59659
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 19:17:16 +0300

    C - Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a sting
+ * @s: string to be printed
+ *
+ * Return: s
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+
+	return (0);
+}
