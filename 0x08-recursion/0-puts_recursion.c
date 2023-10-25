3dab0496558ec4977d534d17b47edcdcb2dd5616
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 19:23:41 +0300

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_puts_recursion(s);
-	_putchar('\n');
+	if (*S == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-	return (0);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
