8155a551c4404746d72967a0d47f41274f950cc1
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 20:48:52 +0300

    C - Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int get_root(int n, int i);
 int is_prime_number(int n);
+int check_prime(int n, int i);
