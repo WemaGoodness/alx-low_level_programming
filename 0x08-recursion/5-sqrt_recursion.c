d08a230a91dd44557279f86772a8e18fb5ec9d88
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 20:26:21 +0300

    C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * get_root - calculates root of n
+ * @n: number to square root
+ * @i: root of n
+ *
+ * Return: root of n
+ */
+int get_root(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+
+	return (get_root(n, i + 1))
+/**
  * _sqrt_recursion - finds natural
  * square root of n
  * @n: number to square root
@@ -9,19 +28,14 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if ((i * i > n) && (n < 0))
+	if (n < 0)
 	{
 		return (-1);
-	}
-	if (i * i == 0)
-	{
-		return (i);
 	}
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	return (_sqrt_recursion(n, i + 1))
+	return (get_root(n, 0))
+}
