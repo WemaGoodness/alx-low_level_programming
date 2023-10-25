e55864b105edc00d37f7574e82f1252b4f184052
WemaGoodness <goodnesswema20@gmail.com>
2023-10-25 21:10:54 +0300

    C - Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * find_lindrome - looks for palindrome
+ * @start: beginning of string
+ * @end: end of string
+ *
+ * Return: 1 or 0
+ */
+int find_lindrome(char *start, achr *end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (*start != *end)
+	{
+		return (0);
+	}
+
+	return (find_lindrome(start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - shows if s is a
+ * palindrome
+ * @s: string to check
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+
+	return (find_lindrome(start + 1, end - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,5 @@ int _sqrt_recursion(int n);
 int get_root(int n, int i);
 int is_prime_number(int n);
 int check_prime(int n, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
