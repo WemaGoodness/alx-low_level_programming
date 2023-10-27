07a911bf47ebb0dd9104b767f7f8ac9ff0aacf34
WemaGoodness <goodnesswema20@gmail.com>
2023-10-27 14:33:33 +0300

    C - Static libraries C

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: string to convert
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int length, sign = 1;
+	unsigned int num = 0;
+	int equiv = 0;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+		if (s[length] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[length] >= '0' && s[length] <= '9')
+		{
+			num = num * 10 + (s[length] - '0');
+			equiv = 1;
+		}
+		else if (equiv == 1)
+		{
+			break;
+		}
+	}
+
+	num *= sign;
+	return (num);
+}
---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: null byte difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
