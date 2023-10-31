fc1aae71973c50e5003ab4edad7dc89c2dc91960
WemaGoodness <goodnesswema20@gmail.com>
2023-10-31 15:56:29 +0300

    C - malloc, free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grind - returns pointer to
+ * alloc_grid - returns pointer to
  * 2 dimensional array
  * @width: shorter side of dimension
  * @height: vertical side of dimension
@@ -11,36 +11,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, **grind;
+	int i, j, **grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	grind = (int **)malloc(height * sizeof(int *));
+	grid = (int **)malloc(height * sizeof(int *));
 
-	if (grind == NULL)
+	if (grid == NULL)
 	{
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
-		grind[i] = (int *)malloc(width * sizeof(int));
-		if (grind[i] == NULL)
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 			{
-				free(grind[j]);
+				free(grid[j]);
 			}
-			free(grind);
+			free(grid);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 		{
-			grind[i][j] = 0;
+			grid[i][j] = 0;
 		}
 	}
-	return (grind);
+	return (grid);
 }
