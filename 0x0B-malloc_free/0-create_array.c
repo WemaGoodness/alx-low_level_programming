c1f369721bfd5fb3e4ffa60ee2176cff84c9c95f
WemaGoodness <goodnesswema20@gmail.com>
2023-10-31 14:48:38 +0300

    C - malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array -creates an array of
+ * chars
+ * @size: number of bytes
+ * @c: character to allocate
+ *
+ * Return: array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *ar = (char*)malloc(size * sizeof(char));
+
+	if (ar != NULL)
+	{
+		for (i = 0; I < size; i++)
+		{
+			ar[i] = c;
+		}
+	}
+
+	return (ar);
+}
