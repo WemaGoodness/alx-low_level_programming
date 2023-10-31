0d2aae9f211bc9b44e83007fa729177ca3a85100
WemaGoodness <goodnesswema20@gmail.com>
2023-10-31 14:53:30 +0300

    C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,14 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *ar;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *ar = (char*)malloc(size * sizeof(char));
+	ar = (char*)malloc(size * sizeof(char));
 
 	if (ar != NULL)
 	{
