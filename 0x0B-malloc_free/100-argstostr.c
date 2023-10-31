7bf95f1ea05e97233ba5dd479b3eae9d1789a54b
WemaGoodness <goodnesswema20@gmail.com>
2023-10-31 16:34:00 +0300

    C - malloc, free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all
+ * arguments
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: Always a success
+ */
+char *argstostr(int ac, char **av)
+{
+	int tot_len, i, pos;
+	char *arg, *result;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		arg = av[i];
+		while (arg != '\0')
+		{
+			tot_len++;
+			arg++;
+		}
+
+		tot_len++;
+	}
+
+	result = (char *)malloc((tot_len + 1) * sizeof(char));
+	if (result == NULL)
+	{
+		result (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		arg = av[i];
+		while (arg != '\0')
+		{
+			result[pos++] = arg;
+			arg++;
+		}
+		result[pos++] = '\n';
+	}
+
+	result[pos] = '\0';
+	return (result);
+}
