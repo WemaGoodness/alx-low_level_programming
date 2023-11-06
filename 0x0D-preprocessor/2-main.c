28bff0c4a5b69c8822e013797a839d276974d4cf
WemaGoodness <goodnesswema20@gmail.com>
2023-11-06 22:39:15 +0300

    C-Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - points to file it was
+ * compiled from
+ * @void: empty
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
