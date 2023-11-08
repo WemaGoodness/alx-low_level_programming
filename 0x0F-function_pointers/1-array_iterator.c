ccc9f14e4918a67721708a67ba719922e6b01a4c
WemaGoodness <goodnesswema20@gmail.com>
2023-11-08 23:52:59 +0300

    C - Function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_itarator - repeats array
+ * @array: array to repeat
+ * @size: size of array
+ * @action: pointer to function
+ *
+ * Return: Always a success
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
