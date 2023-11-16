7086646b0f326f7e092fdbfc97a08a656c86027e
WemaGoodness <goodnesswema20@gmail.com>
2023-11-16 20:23:19 +0300

    C - Singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,6 +3,7 @@
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the head of the list
+ *
  * Return: the number of nodes
  */
 size_t list_len(const list_t *h)
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to pointer to head
+ * @str: string to be duplicated and stored in the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
