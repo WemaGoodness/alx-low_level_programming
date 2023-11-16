aacab2bf9d36fe9d8a6ea260caec55073a21a34d
WemaGoodness <goodnesswema20@gmail.com>
2023-11-16 20:34:36 +0300

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_before_main - prints a message before the main function is executed
+ * @void: nothing
+ *
+ * Return: Success
+ */
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
