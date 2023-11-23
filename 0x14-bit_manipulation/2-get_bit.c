c3e5961586a82f9440567fbe9b33c990df9184c2
WemaGoodness <goodnesswema20@gmail.com>
2023-11-23 23:03:49 +0300

    C - Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of
+ * given index
+ * @ index - index
+ *
+ * Return: value of index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8 - 1))
+	{
+		return (-1);
+	}
+
+	n = n >> index;
+
+	return (n & 1);
+}
