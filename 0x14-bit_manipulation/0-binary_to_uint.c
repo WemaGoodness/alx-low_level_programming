339633d1e85b419eca84d990540f7ac6fe8cec5f
WemaGoodness <goodnesswema20@gmail.com>
2023-11-23 22:50:37 +0300

    C - Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - converts binary to
+ * unsigned int
+ * @b: binary to convert
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i = 0;
+
+	if (b == NULL || *b == '\0')
+	{
+		return (0);
+	}
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		num = num << 1;
+
+		num = num + (b[i] - '0');
+
+		i++;
+	}
+
+	return (num);
+}
+
