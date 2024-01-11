174a50d78703ccfc812121f0ba378625ef8f2adc
WemaGoodness <goodnesswema20@gmail.com>
2024-01-11 22:18:58 +0300

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,26 +11,22 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *current;
+	dlistint_t *new_node, *current;
 	unsigned int i;
 
 	if (h == NULL)
 	{
 		return (NULL);
 	}
-
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-
 	current = *h;
 	for (i = 0; i < idx - 1 && current != NULL; i++)
 	{
 		current = current->next;
 	}
-
 	if (current == NULL)
 	{
 		return (NULL);
