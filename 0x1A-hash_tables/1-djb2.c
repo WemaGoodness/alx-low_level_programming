c4f75fe60fdcd81b3abd9e1480284a3be6987909
WemaGoodness <goodnesswema20@gmail.com>
2024-02-08 00:46:12 +0300

    C - Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -17,5 +17,5 @@ unsigned long int hash_djb2(const unsigned char *str)
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 
-	return hash;
+	return (hash);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,7 +6,7 @@
  * @ht: hash table you want to look into
  * @key: key you are looking for
  *
- * Return: value associated with the element, or NULL if key couldn’t be found
+ * Return: value associated with element, or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
